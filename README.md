34af57c44f03013b311bb27f8b9dfee37c3b3e66
Jin Park <rasmusmoeller@pm.me>
2022-09-14 01:04:15 +0200

    Updated
    Changelog:
    Committed the finished project files of the Oldagram course project.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# oldagram
- Scrimba course project @ Frontend Developer Career Path 
+## Oldagram is a solo project  from The Frontend Developer Career Path from Scrimba 
+
+The project is an Instagram clone using a selfie portrait of amassing artists 
+
+They provide a Figma file design https://www.figma.com/file/h0MKma9TTWzGOMQ9Ia6ROW/Oldagram)
+
+Task requirements :
+
+- Create the first post
+- Use semantic Html
+- Add hover effect to the icons
+- use CSS classes to make the post more readable
+
+Stretch goals:
+
+- Use JavaScript to render out all tree posts
+- increase the likes when double click the post (or single click event)
+
+
+## About Scrimba
+
+At Scrimba their goals are to create the best possible coding school at the cost of a gym membership! 💜
+If we succeed with this, it will give anyone who wants to become a software developer a realistic shot at succeeding, regardless of where they live and the size of their wallets 🎉
+The Frontend Developer Career Path aims to teach you everything you need to become a Junior Developer, or you could take a deep-dive with one of our advanced courses 🚀
+
+- [All courses](https://scrimba.com/allcourses)
+- [The Frontend Career Path](https://scrimba.com/learn/frontend)
+- [Become a Scrimba Pro member](https://scrimba.com/pricing)
+
+Happy Coding!
---
+++ index.js
@@ -0,0 +1,134 @@
+const log = (arg) => console.log(arg);
+
+const posts = [
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+  },
+];
+
+const articlePost = document.querySelector("#article-post");
+
+let renderPosts = () => {
+  let allPost = "";
+
+  posts.map((element, id) => {
+    allPost += `
+
+            <div class="article-detail">
+
+                <img src="${element.avatar}" 
+                    class="avatar avatar-user" 
+                    alt="User ${element.name}" 
+                    title="User ${element.name}" 
+                />
+
+                <div class="article-detail-content">
+
+                    <h3 class="article-title text-bold"> ${element.name}</h3>
+                    
+                    <p  class="small-text">${element.location}</p>
+
+                </div>
+
+            </div>
+
+            <div class="image-content">
+
+                <img src="${element.post}" 
+                    class="post-image"  
+                    alt="Post of ${element.name}" 
+                    title="Post of ${element.name}"
+                />
+
+            </div>
+
+            <footer class="article-footer">
+            
+                <div class="footer-options">    
+                    
+                    <img src="images/icon-heart.png" 
+                            id="icon-heart--${id}" 
+                            class="icon like-icon"  
+                            alt="Icon heart" title="icon-heart" 
+                            onclick="addRemoveLike(${id})"
+                    />
+
+                    <img src="images/icon-comment.png" 
+                        class="icon" alt="icon comment" 
+                        title="icon comment" 
+                    />
+
+                    <img src="images/icon-dm.png" 
+                        class="icon" alt="Direct messege icon" 
+                        title="Direct messege icon" 
+                    />
+
+                </div>
+
+                <p class="text-bold likes " id="likes-count--${id}"> 
+
+                    ${element.likes} likes 
+
+                </p>
+
+                
+                <div class="username-comments">
+    
+                        <p class="text-bold"> ${element.username}        
+                            <span class="small-text">${element.comment}</span>
+                        </p>
+    
+                </div>
+
+            </footer>  
+
+            <div class="breaker"></div>   <!-- /.breaker -->
+        `;
+  });
+
+  articlePost.innerHTML = allPost;
+};
+
+renderPosts();
+
+function addRemoveLike(id) {
+  const iconLikes = document.querySelector(`#icon-heart--${id}`);
+  const likesCountEL = document.querySelector(`#likes-count--${id}`);
+  let likes = posts[id].likes;
+
+  iconLikes.classList.toggle("like-yes");
+
+  if (iconLikes.classList.contains("like-yes")) {
+    likes += 1;
+    likesCountEL.textContent = likes + " likes";
+    iconLikes.src = "images/heart-icon-red.png";
+  } else {
+    likesCountEL.textContent = likes + " likes";
+    iconLikes.src = "images/icon-heart.png";
+  }
+}
